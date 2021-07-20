1a19a1d6d37ee33056c194de0a06ad3723b4dd00
yolandagb <yolandagarciabarranco@hotmail.com>
2021-07-20 11:59:12 +0000

    done all the javaS, we just some css

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,83 +6,107 @@ import "../../styles/home.scss";
 
 //create your first component
 export function Home(props) {
+	const [todo, setTodo] = useState(["Make the bed ", " Wash my hands"]);
+	const [inputValue, setInputValue] = useState("");
+	const toDoListUrl =
+		"https://assets.breatheco.de/apis/fake/todos/user/tibisfly2";
 
+	// const handleClick = () => {
+	//     const newTodo = todo;
+	//     newTodo.push(inputValue);
+	//     setTodo(newTodo);
+	//     console.log(todo, ["Esto es todo"]);
+	//     setInputValue("");
+	// };
+	// const deleteItem = (index, event) => {
+	//     let newTodo = [...todo];
+	//     let removed = newTodo.splice(index, 1);
+	//     setTodo(newTodo);
+	// };
+	function addTodo(e) {
+		let input = document.querySelector("input").value;
 
-    const [todo, setTodo] = useState(["Make the bed ", " Wash my hands"]);
-    const [inputValue, setInputValue] = useState("");
+		if (e.key === "Enter" && input != "") {
+			let newTodos = [...todo, { label: input, done: false }];
+			setTodo(newTodos);
+			document.querySelector("input").value = "";
 
-    const handleClick = () => {
-        const newTodo = todo;
-        newTodo.push(inputValue);
-        setTodo(newTodo);
-        console.log(todo, ["Esto es todo"]);
-        setInputValue("");
-    };
-    // fetch('http://assets.breatheco.de/apis/fake/todos/user/yolandagb', {
-    //     method: "PUT",
-    //     body: JSON.stringify(todos),
-    //     headers: {
-    //         "Content-Type": "application/json"
-    //     }
-    // })
-    //     // .then(resp => {
-    //     //     console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
-    //     //     console.log(resp.status); // el código de estado = 200 o código = 400 etc.
-        //     console.log(resp.text([
-        //         { label: "Make the bed", done: false },
-        //         { label: "Walk the dog", done: false },
-        //         { label: "Do the replits", done: false }
-        //     ])); // Intentará devolver el resultado exacto como cadena (string)
+			fetch(toDoListUrl, {
+				method: "PUT",
+				body: JSON.stringify(newTodos),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(resp => {
+					return resp.json();
+				})
+				.then(data => data)
+				.catch(error => error);
+		}
+	}
 
-        //     return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
-        //     })
-        //     .then(data => {
-        //         //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-        //         console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
-        //     })
-        //     .catch(error => {
-        //         //manejo de errores
-        //         console.log(error);
-        //     }
+	function deleteTodo(elementIndex) {
+		var filtered = todo.filter(function(value, i) {
+			return elementIndex !== i;
+		});
+		setTodo(filtered);
+		fetch(toDoListUrl, {
+			method: "PUT",
+			body: JSON.stringify(filtered),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				return resp.json();
+			})
+			.then(data => data)
+			.catch(error => error);
+	}
 
-    const deleteItem = (index, event) => {
-        let newTodo = [...todo];
-        let removed = newTodo.splice(index, 1);
-        setTodo(newTodo);
-    };
+	function getTodos() {
+		fetch(toDoListUrl, { method: "GET" })
+			.then(response => response.json())
+			.then(responseJSON => {
+				setTodo(responseJSON);
+				console.log(responseJSON);
+			});
+	}
 
-            return (
-                <div className="todo-list">
-                    <div className="add-item-container">
-                        <div className="text-center">Add item</div>
-                        <br />
-                        <input
-                            className="inputs-container"
-                            type="text"
-                            placeholder="What needs to be done"
-                            value={inputValue}
-                            onChange={e => setInputValue(event.target.value)}
-                        />
-                        <button onClick={handleClick} className="add-button">
-                            Add
-				</button>
-                        <br />
-                        <ul>
-                            {todo.map((item, index) => {
-                                return (
-                                    <li className="key" key={index}>
-                                        {item}
-                                        <buttom
-                                            className="delete-button"
-                                            onClick={e => deleteItem(index, event)}>
-                                            X
+	useEffect(() => {
+		getTodos();
+	}, []);
+
+	return (
+		<div className="todo-list">
+			<div className="add-item-container">
+				<div className="text-center">Add item</div>
+				<br />
+				<input
+					onKeyPress={e => {
+						addTodo(e);
+					}}
+				/>
+				<br />
+				<ul>
+					{todo.map((value, index) => {
+						return (
+							<li className="key" key={index}>
+								{value.label}
+								<buttom
+									className="delete-button"
+									onClick={event => deleteTodo(index)}>
+									X
 								</buttom>
-                                    </li>
-                                );
-                            })}
-
-                        </ul>
-                    </div>
-                </div>
-            );
+							</li>
+						);
+					})}
+				</ul>
+				<div className="text-muted pb-1">
+					<p>{todo.length} to do</p>
+				</div>
+			</div>
+		</div>
+	);
 }
